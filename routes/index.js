42df810a212997ccc73c62c750a1542d01b5bd4b
James D Stewart Jr <jamstewjr@gmail.com>
2016-10-11 18:32:44 -0700

    database setup complete.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', (request, response) => {
-  response.send('This is the home page' )
+  response.render('layout' )
 })
 
 module.exports = router
