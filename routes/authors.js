4d4e42183d4f1670c6f9e634a2a82b7604ce5886
James D Stewart Jr <jamstewjr@gmail.com>
2016-10-13 12:06:01 -0700

    started on author support.

--- routes/authors.js
+++ routes/authors.js
@@ -1,9 +1,22 @@
 var express = require('express')
 var router = express.Router()
+const db = require('../database')
 
-
-router.get('/details', (request, response) => {
-  response.send('show author books ')
+router.get('/', (request, response) => {
+  db.getAllAuthors()
+    .then( authors => {
+      response.render('./authors/index', {authors: authors})
+    })
+    .catch( error => {throw error})
 })
+
+router.get('/details/:author_id', (request, response) => {
+  const { author_id } = request.params
+  db.getAuthorById(author_id)
+    .then(author => {
+      response.render('authors/author-details', {author: author})
+    })
+})
+
 
 module.exports = router
